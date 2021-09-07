2f12c39e27b00da8b8d35bf3fdb4ca2c37d4f510
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-06 20:40:22 -0600

    Make merchants items controller

---
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Merchants::ItemsController < ApplicationController
+
+  def index
+    items = Merchant.find(params[:merchant_id]).items.all
+    render json: MerchantItemSerializer.format_items(items)
+  end
+
+end
