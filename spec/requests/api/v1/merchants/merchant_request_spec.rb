270b6c656ac320c04bd454586991b9de8767e75d
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-06 19:59:56 -0600

    Finish spec tests for merchant

--- spec/requests/api/v1/merchants/merchant_request_spec.rb
+++ spec/requests/api/v1/merchants/merchant_request_spec.rb
@@ -9,7 +9,16 @@ describe "Merchant API" do
 
       expect(response).to be_successful
 
-      merchant = JSON.parse(response.body, symbolize_names: true)
+      merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(String)
+
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to be_a(String)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
 end
