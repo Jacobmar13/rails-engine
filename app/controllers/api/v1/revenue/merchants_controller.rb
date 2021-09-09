8ff4f75af28f457f22830789e5da576cffae4066
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-09 17:52:54 -0600

    Give message to error path

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -4,7 +4,8 @@ class Api::V1::Revenue::MerchantsController < ApplicationController
       merchants = Merchant.top_merchants(params[:quantity])
       render json: MerchantRevenueSerializer.new(merchants)
     else
-      render json: ErrorSerializer.error, status: :bad_request
+      error = 'Error, quantity param is missing'
+      render json: ErrorSerializer.error(error), status: :bad_request
     end
   end
 end
