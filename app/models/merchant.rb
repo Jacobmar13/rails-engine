49c1f557e7af2faec3ae68db0d0fdf2af4041529
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-09 15:41:10 -0600

    Create top merchants method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,4 +7,13 @@ class Merchant < ApplicationRecord
   def self.find_merchant(query)
     find_by("lower(name) LIKE lower(?)", "%#{query}%" )
   end
+
+  def self.top_merchants(quantity)
+    joins(items: {invoice_items: {invoice: :transactions}})
+    .select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+    .group(:id)
+    .where(transactions: { result: :success})
+    .order(revenue: :desc)
+    .limit(quantity)
+  end
 end
