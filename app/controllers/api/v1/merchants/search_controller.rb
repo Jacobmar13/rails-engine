51cdd86d524411f406395e4c7b9e4132c2ebcef4
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-08 19:16:33 -0600

    Add search controller with find action

---
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::Merchants::SearchController < ApplicationController
+  def find
+    merchant = Merchant.find_merchant(params[:name])
+    if merchant.class == Merchant
+      render json: MerchantSerializer.new(merchant)
+    else
+      render json: ErrorSerializer.error_object
+    end
+  end
+end
