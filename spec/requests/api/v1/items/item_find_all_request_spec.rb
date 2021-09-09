9d31b28c63e39640353b5d78cf5e93cda9590d21
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-09 12:54:13 -0600

    Make sure spec tests for returning multiple items

--- spec/requests/api/v1/items/item_find_all_request_spec.rb
+++ spec/requests/api/v1/items/item_find_all_request_spec.rb
@@ -9,18 +9,35 @@ describe "Items Find All API" do
 
       expect(response).to be_successful
 
-      item = JSON.parse(response.body, symbolize_names: true)[:data]
+      items = JSON.parse(response.body, symbolize_names: true)[:data]
 
-      expect(item).to have_key(:id)
-      expect(item[:id]).to be_an(String)
+      items.each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_an(String)
 
-      expect(item).to have_key(:type)
-      expect(item[:type]).to be_a(String)
+        expect(item).to have_key(:type)
+        expect(item[:type]).to be_a(String)
 
-      expect(item[:attributes]).to have_key(:name)
-      expect(item[:attributes]).to have_key(:description)
-      expect(item[:attributes]).to have_key(:unit_price)
-      expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes]).to have_key(:merchant_id)
+      end
+    end
+    it 'finds items that matche partial name' do
+      merchant = Merchant.create(name: 'Target')
+      item1 = Item.create(name: 'Cards against Humanity', description: 'Card game', unit_price: 19.99, merchant_id: merchant.id)
+      item2 = Item.create(name: 'Unstable Unicorns Card Game', description: 'Card game', unit_price: 19.99, merchant_id: merchant.id)
+      item3 = Item.create(name: 'Exploding Kittens, A Card Game', description: 'Card game', unit_price: 19.99, merchant_id: merchant.id)
+      item4 = Item.create(name: 'Cinephile, A Card Game', description: 'Card game', unit_price: 19.99, merchant_id: merchant.id)
+
+      get "/api/v1/items/find_all?name=card"
+
+      expect(response).to be_successful
+
+      items = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(items.count).to eq(4)
     end
   end
 end
