d9b1008a8568c27d5b3c4d11fec401f223df0d31
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-08 19:17:22 -0600

    Add class method for search method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: nil
 
   self.per_page = 20
+
+  def self.find_merchant(query)
+    find_by("lower(name) LIKE lower(?)", "%#{query}%" )
+  end
 end
