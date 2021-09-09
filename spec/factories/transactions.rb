9f9ee6e9dd81e18a0ba859ede756b61e8932be2d
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-09 17:51:01 -0600

    Add invoice association

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     credit_card_number { Faker::Number.unique.number(digits: 16) }
     credit_card_expiration_date { "01/25" }
     result { "success" }
+    invoice
   end
 end
