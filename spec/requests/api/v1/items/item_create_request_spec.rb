a4c65fd1463665d188209d0c9be713c52c236550
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-08 20:17:31 -0600

    Add expected to test

--- spec/requests/api/v1/items/item_create_request_spec.rb
+++ spec/requests/api/v1/items/item_create_request_spec.rb
@@ -14,8 +14,12 @@ describe "Item Create API" do
 
       post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
       created_item = Item.last
-      
+
       expect(response).to be_successful
+      expect(created_item.name).to eq(item_params[:name])
+      expect(created_item.description).to eq(item_params[:description])
+      expect(created_item.unit_price).to eq(item_params[:unit_price])
+      expect(created_item.merchant_id).to eq(item_params[:merchant_id])
     end
   end
 end
