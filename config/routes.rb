24a59287d8750a17b6c565104ebcced063859597
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-08 19:17:59 -0600

    Add new route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'search#find'
+      end
       resources :merchants, only: [:index, :show] do
         get '/items', to: 'merchants/items#index'
       end
