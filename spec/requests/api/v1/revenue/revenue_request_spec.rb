627672ced59f1d73d539821708fa1b9283a78074
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-09 14:33:47 -0600

    Update test to have default result

--- spec/requests/api/v1/revenue/revenue_request_spec.rb
+++ spec/requests/api/v1/revenue/revenue_request_spec.rb
@@ -5,9 +5,13 @@ RSpec.describe 'Revenue API' do
     it 'returns top ten merchants by revenue' do
       merchants = create_list(:merchant, 20)
 
-      get '/api/v1/revenue/merchants?quantity=10'
+      get '/api/v1/revenue/merchants'
 
       expect(response).to be_successful
+
+      merchants_returned = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(merchants_returned.count).to eq(10)
     end
   end
 end
