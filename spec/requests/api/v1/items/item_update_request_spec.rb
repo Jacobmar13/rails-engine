35bf7463d9bb48df14b3e6d51e1b58099f969338
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-08 21:47:30 -0600

    Test for sad path

--- spec/requests/api/v1/items/item_update_request_spec.rb
+++ spec/requests/api/v1/items/item_update_request_spec.rb
@@ -15,5 +15,14 @@ RSpec.describe 'Item Update Api' do
       expect(item.name).to_not eq(previous_name)
       expect(item.name).to eq('Sleepytime Tea')
     end
+    it 'returns an error page with bad merchant id' do
+      id = create(:item).id
+      item_params = { merchant_id: -1 }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+
+      expect(response).to be_a_bad_request
+    end
   end
 end
