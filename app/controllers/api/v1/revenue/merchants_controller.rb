a3c7f658b8c943720f5757c3d3ff3f043e9c261e
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-09 15:40:57 -0600

    Add top earners action

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::Revenue::MerchantsController < ApplicationController
   def top_earners
-    
+    if params[:quantity]
+      merchants = Merchant.top_merchants(params[:quantity])
+      render json: MerchantRevenueSerializer.new(merchants)
+    else
+      render json: ErrorSerializer.error, status: :bad_request
+    end
   end
 end
