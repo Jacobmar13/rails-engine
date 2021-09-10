baa2caf662886011bb48e79236885dc0059d2cf0
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-10 00:00:34 -0600

    Edit spec test to try to capture all associations

--- spec/requests/api/v1/merchants/merchant_items_sold_request_spec.rb
+++ spec/requests/api/v1/merchants/merchant_items_sold_request_spec.rb
@@ -3,20 +3,13 @@ require 'rails_helper'
 describe "Merchant by Items Sold API" do
   describe 'basic api call' do
     it "give list of merchants ordered by items sold with quantity param" do
-      create_list(:merchant, 20)
-      merchant1 = Merchant.first
-      merchant2 = Merchant.second
-      merchant3 = Merchant.third
-      merchant4 = Merchant.fourth
-      merchant5 = Merchant.fifth
+      merchant1 = Merchant.create(name: 'Target')
+      merchant2 = Merchant.create(name: 'Walmart')
+      merchant3 = Merchant.create(name: 'HEB')
 
-      create_list(:item, 8, merchant_id: merchant4.id)
-      create_list(:item, 6, merchant_id: merchant2.id)
-      create_list(:item, 5, merchant_id: merchant5.id)
-      create_list(:item, 3, merchant_id: merchant1.id)
-      create_list(:item, 2, merchant_id: merchant3.id)
+      
 
-      get "/api/v1/merchants/most_items?quantity=6"
+      get "/api/v1/merchants/most_items?quantity=3"
 
       expect(response).to be_successful
 
