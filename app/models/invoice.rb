844b97161074b5a96be154d40b3fc24a8e5e39e8
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-09 22:06:55 -0600

    Add new method for total revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,13 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
   has_many :transactions, dependent: nil
   has_many :invoice_items, dependent: nil
+
+  def self.total_revenue
+    joins(:transactions, :invoice_items)
+    .where(transactions: { result: :success})
+    .select("sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+    .group('invoices.id').sum do |revenue|
+      revenue.revenue
+    end
+  end
 end
